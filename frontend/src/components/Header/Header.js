fad19723a00e2f41ac6b3e764ac14e9a335bda2a
AnaLara714 <Analara714@gmail.com>
2023-11-28 11:19:37 -0300

    feat: estuturando o frontend

---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import "./Header.style.css";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <h2 className="title">ENCONTRE SEU MELHOR PLANO DE ESTUDOS</h2>
+    </header>
+  );
+}
