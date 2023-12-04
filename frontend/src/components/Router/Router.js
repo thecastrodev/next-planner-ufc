c131d3079436edb785f042ef418ff08a2a7552ab
AnaLara714 <Analara714@gmail.com>
2023-12-04 10:18:43 -0300

    feat: tela login estilizada

--- frontend/src/components/Router/Router.js
+++ frontend/src/components/Router/Router.js
@@ -1,11 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import Login from "../../pages/Login/Login";
 
 export function Router() {
   return (
     <BrowserRouter>
       <Routes>
-
+        <Route path={"/"} element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
