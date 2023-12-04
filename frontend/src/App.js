b0afe5c7942cf4fae25c35e563ef0216934eead8
AnaLara714 <Analara714@gmail.com>
2023-12-04 09:20:35 -0300

    feat: adicionando rotas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
-import Main from "./components/Main/Main";
+import { Router } from "./components/Router/Router";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Main />
-      <Footer />
+      <Router />
     </div>
   );
 }
--- frontend/src/components/Main/Main.js
+++
@@ -1,5 +0,0 @@
-import "./Main.style.css";
-
-export default function Main() {
-  return <div className="main"></div>;
-}
